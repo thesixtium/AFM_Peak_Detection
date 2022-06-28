c41a9cfd20134d8cb37ead74875f6c66215a64bb
aberezowski <ajrberezowski@gmail.com>
2022-06-28 16:36:39 -0600

    More Changes!

--- print_results.py
+++ print_results.py
@@ -13,9 +13,30 @@ def print_results(results, filename):
         for set in data:
             f.write(set + ": ")
             f.write("\n")
-            for result in results[type + " " + set + " Peaks"]:
+            f.write("\tValues")
+            f.write("\n")
+            result_set = results[type + " " + set + " Peaks"]
+            for result in result_set:
                 f.write("\t- (" + str(result[0]) + ", " + str(result[1]) + ")")
                 f.write("\n")
+            if len(result_set) > 1:
+                f.write("\tValue Math")
+                f.write("\n")
+                for i in range(1, len(result_set)):
+                    f.write("\t- Result " +
+                            str(i) +
+                            "'s x-value is " +
+                            str(round(result_set[i][0] / result_set[i-1][0], 2)) +
+                            "x result " + str(i-1) +
+                            "'s x-value")
+                    f.write("\n")
+                    f.write("\t- Result " +
+                            str(i) +
+                            "'s y-value is " +
+                            str(round(result_set[i][1] / result_set[i - 1][1], 2)) +
+                            "x result " + str(i - 1) +
+                            "'s y-value")
+                    f.write("\n")
             f.write("\n")
             f.write("\n")
 
